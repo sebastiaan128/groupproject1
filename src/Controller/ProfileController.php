85fced5a8b5d927f0a2be93406a107396a7144fe
Sebastiaan Balfoort <balfoortsebastiaan@gmail.com>
2026-03-16 12:08:50 +0100

    SQL INJECTION removed

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -12,6 +12,21 @@ use App\Entity\Profiel;
 
 class ProfileController extends AbstractController
 {
+    private function looksLikeSqli(string $value): bool
+    {
+        $patterns = [
+            "/('|--|;|\/\*|\*\/)/",
+            '/\b(select|insert|update|delete|drop|union|exec|execute|create|alter|truncate|cast|convert|declare)\b/i',
+            '/\b(or|and)\s+[\w\'"]+\s*=\s*[\w\'"]+/i',
+            '/xp_/i',
+            '/0x[0-9a-f]+/i',
+        ];
+        foreach ($patterns as $pattern) {
+            if (preg_match($pattern, $value)) return true;
+        }
+        return false;
+    }
+
     #[Route('/profile', name: 'profile')]
     public function index(Request $request, EntityManagerInterface $em): Response
     {
@@ -26,13 +41,23 @@ class ProfileController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $fields = [$profiel->getName(), $profiel->getEmail(), $profiel->getBio(), $profiel->getStudie()];
+            foreach ($fields as $value) {
+                if ($value !== null && $this->looksLikeSqli($value)) {
+                    return $this->render('profile.html.twig', [
+                        'form'        => $this->createForm(ProfileType::class, $profiel),
+                        'profiel'     => $profiel,
+                        'sql_attempt' => true,
+                    ]);
+                }
+            }
             $em->flush();
             $request->getSession()->set('user_name', $profiel->getName());
             return $this->redirectToRoute('profile');
         }
 
         return $this->render('profile.html.twig', [
-            'form' => $form,
+            'form'    => $form,
             'profiel' => $profiel,
         ]);
     }
